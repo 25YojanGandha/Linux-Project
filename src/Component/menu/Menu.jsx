11110238f1c4ef136bcd4552c8eea6f8536456e8
arpit-maurya <arpitmaurya161@gmail.com>
2022-01-26 19:06:31 +0530

    menu css update

--- src/Component/menu/Menu.jsx
+++ src/Component/menu/Menu.jsx
@@ -1,36 +1,66 @@
 import { useContext, useState } from 'react';
 import { GlobalData } from '../../App';
 import './Menu.css';
-import { Input } from '@nextui-org/react';
+// import { Input } from '@nextui-org/react';
 
 function Menu() {
-    let gData = useContext(GlobalData);
-    let [applicationsArray, setapplicationsArray] = useState(['Browser', 'Camera', 'Gallery', 'Clock', 'Terminal', 'Account', 'Appstore', 'Help', 'Powerpoint', 'Settings', 'Meet', 'Drive', 'Game', 'Calender', 'Maps', 'Weather','Musicplayer','Hillclimb']);
+  let gData = useContext(GlobalData);
+  let [applicationsArray, setApplicationsArray] = useState([
+    'Browser',
+    'Gallery',
+    'Clock',
+    'Terminal',
+    'Account',
+    'Camera',
+    'Appstore',
+    'Help',
+    'Settings',
+    'Meet',
+    'Clock',
+    'Notepad',
+    'Chess',
+    'Vscode',
+    'Calender',
+    'Maps',
+    'Hillclimb',
+    'trash',
+    'Weather',
+    'Music',
+    'Message',
+  ]);
 
-    return (
-        <div className='menu-main-container'>
-            <div className='menu-search-bar'>
-                <input type="text" placeholder='Search'/>
-                {/* <Input
-                    clearable
-                    bordered
-                    color="primary"
-                    labelPlaceholder="Loading..."
-                    // contentRight={<Loading size="xs" />}
-                /> */}
-            </div>
-            <div className='menu-app-container'>
-
-                {applicationsArray.map((appName) => {
-                    return <div className='app-container-card'>
-                        <img src={`./images/${appName}.png`} alt={appName} />
-                        <div className='app-text'>{appName}</div>
-                    </div>;
-                })}
-
-            </div>
+  return (
+    <div className='menu-main-container'>
+      <div className='menu-search-bar'>
+        <div className='inputContainer'>
+          <svg xmlns='http://www.w3.org/2000/svg' height='16' width='16'>
+            <path
+              d='M7 1C3.69 1 1 3.69 1 7s2.69 6 6 6a5.948 5.948 0 0 0 3.664-1.273l2.863 2.863 1.063-1.063-2.863-2.863A5.949 5.949 0 0 0 13 7c0-3.31-2.69-6-6-6zm0 1a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5z'
+              className='searchIcon'
+              color='#000'
+              font-weight='400'
+              font-family='sans-serif'
+              white-space='normal'
+              overflow='visible'
+              fill='gray'
+            />
+          </svg>
+          <input placeholder='Type To Search' type='text' />
         </div>
-    );
+      </div>
+
+      <div className='menu-app-container'>
+        {applicationsArray.map((appName) => {
+          return (
+            <div className='app-container-card'>
+              <img src={`./images/${appName}.png`} alt={appName} />
+              <div className='app-text'>{appName}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
